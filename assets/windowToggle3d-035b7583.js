f09f6884a2ffa07ec6e59be5228fa3e729f5b732
theOlchi <werwils@hotmail.de>
2024-01-11 09:55:22 +0100

    Updates

--- assets/windowToggle3d-035b7583.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./index-e6384a2c.js";window.toggle3d=function(o){t[o]||(t[o]=!1),t[o]=!t[o]};
---
+++ assets/windowToggle3d-a03ab129.js
@@ -0,0 +1,1 @@
+import{s as t}from"./index-89fb4274.js";window.toggle3d=function(o){t[o]||(t[o]=!1),t[o]=!t[o]};
